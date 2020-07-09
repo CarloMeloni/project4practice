5297fbd901e5cf6ff0663473c1aa7c7b2e946af4
carloMeloni <carlo.meloni@hotmail.it>
2020-07-09 16:32:31 +0200

    Adding a checkout page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Layout from "./containers/Layout/layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+import Checkout from "./containers/Checkout/Checkout";
+import CheckoutSummary from "./components/Order/CheckoutSummary/CheckoutSummary";
 
 class App extends Component {
   render() {
@@ -8,6 +10,7 @@ class App extends Component {
       <div>
         <Layout>
           <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary";
+
+const CheckoutSummary = (props) => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>Hope it tastes well!</h1>
+      <div style={{ width: "300px", height: "300px", margin: "auto" }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger" clicked>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked>
+        CONTINUE
+      </Button>
+    </div>
+  );
+};
+
+export default CheckoutSummary;
